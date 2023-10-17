d5e74990d2f3bd5c58712a68d223f6ada46cbdac
jsjune <avraskio@gmail.com>
2023-10-17 16:51:20 +0900

    init

---
+++ src/main/java/com/example/health/HealthController.java
@@ -0,0 +1,30 @@
+package com.example.health;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+public class HealthController {
+    private final HealthRepository healthRepository;
+
+    @GetMapping("/health")
+    public String health() {
+        return "ok-v3\n";
+    }
+
+    @GetMapping("/health/save")
+    public String save() {
+        healthRepository.save(new Health(UUID.randomUUID().toString()));
+        return "save\n";
+    }
+
+    @GetMapping("/health/{id}")
+    public Health getHealth(@PathVariable Long id) {
+        return healthRepository.findById(id).orElse(null);
+    }
+}
