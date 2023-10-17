5545b6a67f4acaddbfd4881cdc8b94e1aa7481b1
jsjune <avraskio@gmail.com>
2023-10-18 02:54:29 +0900

    k8s update

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,11 @@ COPY build/libs/health-0.0.1-SNAPSHOT.jar app.jar
 
 # 환경 변수 설정
 ENV SPRING_PROFILES_ACTIVE=production
-ENV DATABASE_URL=jdbc:mysql://mysql-master-service:3306/battingGround
-ENV DATABASE_USERNAME=root
-ENV DATABASE_PASSWORD=1234
+ENV SPRING_DATASOURCE_MASTER_URL=jdbc:mysql://mysql-master-service:3306/battingGround?allowPublicKeyRetrieval=true&useSSL=false
+ENV SPRING_DATASOURCE_MASTER_USERNAME=root
+ENV SPRING_DATASOURCE_MASTER_PASSWORD=1234
+ENV SPRING_DATASOURCE_SLAVE_URL=jdbc:mysql://mysql-slave-service:3306/battingGround?allowPublicKeyRetrieval=true&useSSL=false
+ENV SPRING_DATASOURCE_SLAVE_USERNAME=root
+ENV SPRING_DATASOURCE_SLAVE_PASSWORD=1234
 
 ENTRYPOINT ["java", "-jar", "app.jar"]
---
+++ src/main/java/com/example/health/ignore/RoutingDataSource.java
@@ -0,0 +1,24 @@
+package com.example.health.ignore;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+@RequiredArgsConstructor
+@Slf4j
+public class RoutingDataSource extends AbstractRoutingDataSource {
+    private final DataSourceKey dataSourceKey;
+
+    @Override
+    protected Object determineCurrentLookupKey() {
+        boolean isReadOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+        if (isReadOnly) {
+            log.info("Connection Slave");
+            return dataSourceKey.getDefaultSlaveKey();
+        } else {
+            log.info("Connection Master");
+            return dataSourceKey.getMasterKey();
+        }
+    }
+}
