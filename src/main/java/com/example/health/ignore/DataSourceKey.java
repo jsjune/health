90026380e361c8cd2f3325135ef1d8d6247182e2
jsjune <avraskio@gmail.com>
2023-10-18 12:26:49 +0900

    k8s update

--- src/main/java/com/example/health/ignore/DataSourceKey.java
+++ src/main/java/com/example/health/ignore/DataSourceKey.java
@@ -1,27 +1,27 @@
-package com.example.health.ignore;
-
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Component;
-
-@Component
+//package com.example.health.ignore;
+//
+//import org.springframework.context.annotation.Profile;
+//import org.springframework.stereotype.Component;
+//
+//@Component
 //@Profile("production")
-public class DataSourceKey {
-    private static final String MASTER_KEY = "master";
-    private static final String SLAVE_KEY = "slave";
-
-    private static final String INDENT = "_";
-    private static final int DEFAULT_SLAVE_NUMBER = 1;
-
-
-    public String getMasterKey() {
-        return MASTER_KEY;
-    }
-
-    public String getSlaveKeyByNumber(int idx) {
-        return SLAVE_KEY + INDENT + idx;
-    }
-
-    public String getDefaultSlaveKey() {
-        return SLAVE_KEY + INDENT + DEFAULT_SLAVE_NUMBER;
-    }
-}
+//public class DataSourceKey {
+//    private static final String MASTER_KEY = "master";
+//    private static final String SLAVE_KEY = "slave";
+//
+//    private static final String INDENT = "_";
+//    private static final int DEFAULT_SLAVE_NUMBER = 1;
+//
+//
+//    public String getMasterKey() {
+//        return MASTER_KEY;
+//    }
+//
+//    public String getSlaveKeyByNumber(int idx) {
+//        return SLAVE_KEY + INDENT + idx;
+//    }
+//
+//    public String getDefaultSlaveKey() {
+//        return SLAVE_KEY + INDENT + DEFAULT_SLAVE_NUMBER;
+//    }
+//}
--- src/main/java/com/example/health/ignore/RoutingDataSource.java
+++ src/main/java/com/example/health/ignore/RoutingDataSource.java
@@ -1,26 +1,26 @@
-package com.example.health.ignore;
-
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Profile;
-import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
-import org.springframework.transaction.support.TransactionSynchronizationManager;
-
-@RequiredArgsConstructor
-@Slf4j
+//package com.example.health.ignore;
+//
+//import lombok.RequiredArgsConstructor;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.context.annotation.Profile;
+//import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+//import org.springframework.transaction.support.TransactionSynchronizationManager;
+//
+//@RequiredArgsConstructor
+//@Slf4j
 //@Profile("production")
-public class RoutingDataSource extends AbstractRoutingDataSource {
-    private final DataSourceKey dataSourceKey;
-
-    @Override
-    protected Object determineCurrentLookupKey() {
-        boolean isReadOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
-        if (isReadOnly) {
-            log.info("Connection Slave");
-            return dataSourceKey.getDefaultSlaveKey();
-        } else {
-            log.info("Connection Master");
-            return dataSourceKey.getMasterKey();
-        }
-    }
-}
+//public class RoutingDataSource extends AbstractRoutingDataSource {
+//    private final DataSourceKey dataSourceKey;
+//
+//    @Override
+//    protected Object determineCurrentLookupKey() {
+//        boolean isReadOnly = TransactionSynchronizationManager.isCurrentTransactionReadOnly();
+//        if (isReadOnly) {
+//            log.info("Connection Slave");
+//            return dataSourceKey.getDefaultSlaveKey();
+//        } else {
+//            log.info("Connection Master");
+//            return dataSourceKey.getMasterKey();
+//        }
+//    }
+//}
